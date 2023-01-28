72c7a6ff7c552ee8c83ec5a2c941c19851d6c019
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-01-28 12:00:27 +0530

    initial commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import OptionSelection from './components/OptionSelection'
 
 function App() {
   
 
   return (
     <div className="App">
-
+      <OptionSelection />
     </div>
   )
 }
