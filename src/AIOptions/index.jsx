d630e13e76808358a0415454204f061195706486
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-01-29 05:38:21 +0530

    option parameters changes

--- src/AIOptions/index.jsx
+++ src/AIOptions/index.jsx
@@ -1,100 +1,93 @@
 export const arrayItems = [
     {
-    name: 'Q&A',
-    id: 'qna',
-    description: 'Answewr questions based on your knowledge base',
-    option:{
+      name: "Q&A",
+      id: "q&a",
+      description: "Answer questions based on existing knowledge",
+      option: {
         model: "text-davinci-003",
         temperature: 0,
         max_tokens: 100,
         top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-        stop: ["\n"],
-    }
-
-},
-{
-    name: "Grammer Correction",
-    id: "grammerCorrection",
-    description: "Correct grammer mistakes in your text",
-    option:{
+      },
+    },
+    {
+      name: "Grammer Correction",
+      id: "grammerCorrection",
+      description: "Corrects sentences into standard English.",
+      option: {
         model: "text-davinci-003",
-        // prompt: "Correct this to standard English:\n\nShe no went to the market.",
         temperature: 0,
-        max_tokens: 60,
-        top_p: 1.0,
+        max_tokens: 100,
+        top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-    }
-},
-{
-    name: "Summarise for a 2nd grader", 
-    id: "summary",
-    description: "Translates difficult text into simple language",
-    option:{
+      },
+    },
+    {
+      name: "Summarize for a 2nd grader",
+      id: "summary",
+      description: "Translates difficult text into simpler concepts.",
+      option: {
         model: "text-davinci-003",
-        // prompt: "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
         temperature: 0.7,
         max_tokens: 64,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-    }
-},
-{
-    name: "English to other languages",
-    id: "translate",
-    description: "Translates text into other languages",
-    option:{
+      },
+    },
+    {
+      name: "English to Other languages",
+      id: "translate",
+      description: "Translates English text into French, Spanish and Japanese.",
+      option: {
         model: "text-davinci-003",
         temperature: 0.3,
         max_tokens: 100,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-    }
-},
-{
-    name: "Movie to emoji",
-    id: "emoji",
-    description: "Translates movie titles into emojis",
-    option:{
+      },
+    },
+    {
+      name: "Movie to Emoji",
+      id: "movieToEmoji",
+      description: "Convert movie titles into emoji.",
+      option: {
         model: "text-davinci-003",
-        temperature: 0.8,
-        max_tokens: 60,
-        top_p: 1.0,
+        temperature: 0,
+        max_tokens: 100,
+        top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-        stop: ["\n"],
-    }
-},
-{
-    name: "Explain code",
-    id: "expcode",
-    description: "Explains code in plain english",
-    option:{
+      },
+    },
+    {
+      name: "Explain code",
+      id: "explainCode",
+      description: "Explain a complicated piece of code.",
+      option: {
         model: "code-davinci-002",
         temperature: 0,
         max_tokens: 64,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-        stop: ["\"\"\""],
-    }
-
-},
-{
-    name: "JavaScript to Python",
-    id: "js2py",
-    description: "Translates JavaScript code into Python",
-    option:{
+      },
+    },
+    {
+      name: "JavaScript to Python",
+      id: "jstopy",
+      description: "Convert simple JavaScript expressions into Python.",
+      option: {
         model: "code-davinci-002",
         temperature: 0,
         max_tokens: 64,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
-    }
-},
-]
+      },
+    },
+  ];
